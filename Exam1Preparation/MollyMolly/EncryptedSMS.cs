295b81dfb2242dc5eaadcfb9bba3aaa6d4a5ef74
Dilyan Traykov <dtraykov94@gmail.com>
2015-01-12 22:05:59 +0200

    Finally Solved The 2-4-8 Task

--- Exam1Preparation/MollyMolly/EncryptedSMS.cs
+++ Exam1Preparation/MollyMolly/EncryptedSMS.cs
@@ -4,31 +4,23 @@ class EncryptedSMS
 {
     static void Main()
     {
-        string input1 = Console.ReadLine();
-        string input2 = Console.ReadLine();
-        string input3 = Console.ReadLine();
-        uint A, B, C;
-        uint R = 0;
-        if ((uint.TryParse(input1, out A)) && (uint.TryParse(input2, out B)) && (uint.TryParse(input3, out C)))
+        long A = long.Parse(Console.ReadLine());
+        long B = long.Parse(Console.ReadLine());
+        long C = long.Parse(Console.ReadLine());
+        long R = 0;
+        if (B == 2)
         {
-            if (B == 2)
-            {
-                R = A % C;
-            }
-            else if (B == 4)
-            {
-                R = A + C;
-            }
-            else if (B == 6)
-            {
-                R = A * C;
-            }
-            else
-            {
-                return;
-            }
-            Console.WriteLine((R % 4 == 0) ? (R / 4) : (R % 4));
-            Console.WriteLine(R);
+            R = A % C;
         }
+        else if (B == 4)
+        {
+            R = A + C;
+        }
+        else if (B == 8)
+        {
+            R = A * C;
+        }
+        Console.WriteLine((R % 4 == 0) ? (R / 4) : (R % 4));
+        Console.WriteLine(R);
     }
 }
