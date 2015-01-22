ddb8b09c55bbcb42d04f3e8c4b4841f341169ab3
Dilyan Traykov <dtraykov94@gmail.com>
2015-01-22 04:42:52 +0200

    C# I Exam Preparation

---
+++ C# I Exam Preparation/MollyMolly/EncryptedSMS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class EncryptedSMS
+{
+    static void Main()
+    {
+        long A = long.Parse(Console.ReadLine());
+        long B = long.Parse(Console.ReadLine());
+        long C = long.Parse(Console.ReadLine());
+        long R = 0;
+        if (B == 2)
+        {
+            R = A % C;
+        }
+        else if (B == 4)
+        {
+            R = A + C;
+        }
+        else if (B == 8)
+        {
+            R = A * C;
+        }
+        Console.WriteLine((R % 4 == 0) ? (R / 4) : (R % 4));
+        Console.WriteLine(R);
+    }
+}
