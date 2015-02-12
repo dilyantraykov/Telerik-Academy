e9fdf5c9ddca7809bc790c8d326a98d7b07c44f5
Dilyan Traykov <dtraykov94@gmail.com>
2015-02-12 03:44:31 +0200

    C# II Exam Preparation

---
+++ C# II Exam Preparation/BunnyFactory/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        //StreamReader reader = new StreamReader("..\\..\\sample-input.txt");
+        //Console.SetIn(reader);
+
+
+        List<int> cages = new List<int>();
+        while(true)
+        {
+            string input = Console.ReadLine();
+            if (input == "END")
+            {
+                break;
+            }
+            else
+            {
+                cages.Add(int.Parse(input));
+            }
+        }
+        int i = 1;
+        while (true)
+        {
+            if (cages.Count < i)
+            {
+                break;
+            }
+
+            int sum1 = 0;
+            int sum2 = 0;
+            BigInteger product1 = 1;
+
+            for (int j = 0; j < i; j++)
+            {
+                sum1 += cages[j];
+            }
+
+            if (cages.Count - sum1 < i)
+            {
+                break;
+            }
+
+            for (int j = i; j < i+sum1; j++)
+            {
+                sum2 += cages[j];
+                product1 *= cages[j];
+            }
+
+            string next = sum2.ToString() + product1.ToString();
+            for (int j = sum1 + i; j < cages.Count; j++)
+            {
+                next += cages[j].ToString();
+            }
+
+            cages.Clear();
+            foreach (var letter in next)
+            {
+                int digit = int.Parse(letter.ToString());
+                if (digit != 0 && digit != 1)
+                {
+                    cages.Add(digit);
+                }
+            }
+            i++;
+        }
+        foreach (var item in cages)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
---
+++ C# II Exam Preparation/TRES4Numbers/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        BigInteger N = BigInteger.Parse(Console.ReadLine());
+        List<int> digits = new List<int>();
+        while(N > 0)
+        {
+            int digit = (int)(N % 9);
+            digits.Add(digit);
+            N /= 9;
+        }
+        digits.Reverse();
+        List<string> tres4 = new List<string>();
+        foreach (var digit in digits)
+        {
+            switch(digit)
+            {
+                case 0:
+                    tres4.Add("LON+");
+                    break;
+                case 1:
+                    tres4.Add("VK-");
+                    break;
+                case 2:
+                    tres4.Add("*ACAD");
+                    break;
+                case 3:
+                    tres4.Add("^MIM");
+                    break;
+                case 4:
+                    tres4.Add("ERIK|");
+                    break;
+                case 5:
+                    tres4.Add("SEY&");
+                    break;
+                case 6:
+                    tres4.Add("EMY>>");
+                    break;
+                case 7:
+                    tres4.Add("/TEL");
+                    break;
+                case 8:
+                    tres4.Add("<<DON");
+                    break;
+            }
+        }
+
+        if (digits.Count == 0)
+        {
+            tres4.Add("LON+");
+        }
+
+        foreach (var word in tres4)
+        {
+            Console.Write(word);
+        }
+    }
+}
