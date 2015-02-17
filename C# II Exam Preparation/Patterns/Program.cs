b8a64191ac3a3d5ba9c814d872f37712e32931e8
Dilyan Traykov <dtraykov94@gmail.com>
2015-02-17 03:11:32 +0200

    C# II Exam Preparation

---
+++ C# II Exam Preparation/Patterns/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // StreamReader reader = new StreamReader("..\\..\\sample-input.txt");
+        // Console.SetIn(reader);
+
+        int N = int.Parse(Console.ReadLine());
+        int[,] numbers = new int[N,N];
+        for (int row = 0; row < N; row++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            for (int col = 0; col < input.Length; col++)
+            {
+                numbers[row, col] = Convert.ToInt32(input[col]);
+            }
+        }
+        long patternSum = 0;
+        bool foundPattern = false;
+        for (int row = 0; row < N - 2; row++)
+        {
+            for (int col = 0; col < N - 4; col++)
+            {
+                if (numbers[row, col] == numbers[row, col + 1] - 1)
+                {
+                    if (numbers[row, col + 1] == numbers[row, col + 2] - 1)
+                    {
+                        if (numbers[row, col + 2] == numbers[row + 1, col + 2] - 1)
+                        {
+                            if (numbers[row + 1, col + 2] == numbers[row + 2, col + 2] - 1)
+                            {
+                                if (numbers[row + 2, col + 2] == numbers[row + 2, col + 3] - 1)
+                                {
+                                    if (numbers[row + 2, col + 3] == numbers[row + 2, col + 4] - 1)
+                                    {
+                                        foundPattern = true;
+                                        int tempSum = (numbers[row, col] + numbers[row, col + 1] + numbers[row, col + 2] + 
+                                            numbers[row + 1, col + 2] + numbers[row + 2, col + 2] + numbers[row + 2, col + 3] + numbers[row + 2, col + 4]);
+                                        if (tempSum > patternSum) 
+                                        {
+                                            patternSum = tempSum;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (foundPattern)
+        {
+            Console.WriteLine("YES {0}", patternSum);
+        }
+        else
+        {
+            patternSum = 0;
+            for (int row = 0; row < N; row++)
+            {
+                for (int col = 0; col < N; col++)
+                {
+                    if (row == col)
+                    {
+                        patternSum += numbers[row, col];
+                    }
+                }
+            }
+            Console.WriteLine("NO {0}", patternSum);
+        }
+        //for (int i = 0; i < N; i++)
+        //{
+        //    for (int j = 0; j < N; j++)
+        //    {
+        //        Console.Write(numbers[i, j] + " ");
+        //    }
+        //    Console.WriteLine();
+        //}
+    }
+}
---
+++ C# II Exam Preparation/VariableLengthCodes/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        //StreamReader reader = new StreamReader("..\\..\\sample-input.txt");
+        //Console.SetIn(reader);
+
+        char[] split = { ' ' };
+        string[] input = Console.ReadLine().Split(split, StringSplitOptions.RemoveEmptyEntries);
+        int L = int.Parse(Console.ReadLine());
+        string[,] codeTable = new string[L,2];
+        for (int i = 0; i < L; i++)
+        {
+            string temp = Console.ReadLine();
+            codeTable[i, 0] = temp[0].ToString();
+            for (int j = 1; j < temp.Length; j++)
+            {
+                codeTable[i, 1] += temp[j];
+            }
+        }
+
+        string encodedText = "";
+        for (int i = 0; i < input.Length; i++)
+        {
+            string tempNumber = Convert.ToString(int.Parse(input[i]), 2);
+            int length = 0;
+            if (tempNumber.Length % 8 == 0)
+            {
+                length = (tempNumber.Length / 8) * 8;
+            }
+            else
+            {
+                length = ((tempNumber.Length / 8) + 1) * 8;
+            }
+            encodedText += tempNumber.PadLeft(length, '0');
+        }
+
+        string[] finalText = encodedText.Split('0');
+
+        for (int i = 0; i < L; i++)
+        {
+            string currentString = "";
+            for (int j = int.Parse(codeTable[i, 1]); j > 0; j--)
+			{
+                currentString += "1";
+			}
+            for (int k = 0; k < finalText.Length; k++)
+            {
+                if (finalText[k] == currentString)
+                {
+                    finalText[k] = codeTable[i, 0];
+                }
+            }
+        }
+
+        foreach (var item in finalText)
+        {
+            Console.Write(item);
+        }
+    }
+}
