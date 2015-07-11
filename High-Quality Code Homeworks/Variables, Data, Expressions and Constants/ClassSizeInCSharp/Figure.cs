7cfa575d9e0b8a66ce9414eafb713ccb70836882
Dilyan Traykov <dtraykov94@gmail.com>
2015-07-12 02:50:51 +0300

    'Variables, Data, Expressions and Constants' Homework Added

---
+++ High-Quality Code Homeworks/Variables, Data, Expressions and Constants/ClassSizeInCSharp/Figure.cs
@@ -0,0 +1,70 @@
+﻿namespace Figures
+{
+    using System;
+
+    public class Figure
+    {
+        private double width;
+        private double height;
+
+        public Figure(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Width");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Height");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public static Figure GetRotatedFigure(Figure figure, double angleOfRotation)
+        {
+            var absoluteCosine = Math.Abs(Math.Cos(angleOfRotation));
+            var absoluteSine = Math.Abs(Math.Sin(angleOfRotation));
+
+            var cosineTimesWidth = absoluteCosine * figure.width;
+            var sineTimesHeight = absoluteSine * figure.height;
+            var sineTimesWidth = absoluteSine * figure.width;
+            var cosineTimesHeight = absoluteCosine * figure.height;
+
+            var figureWidth = cosineTimesWidth + sineTimesHeight;
+            var figureHeight = sineTimesWidth + cosineTimesHeight;
+
+            var rotatedFigure = new Figure(figureWidth, figureHeight);
+
+            return rotatedFigure;
+        }
+    }
+}
---
+++ High-Quality Code Homeworks/Variables, Data, Expressions and Constants/Statistics/Statistics.cs
@@ -0,0 +1,55 @@
+﻿namespace Statistics
+{
+    using System;
+
+    public class Statistics
+    {
+        public void PrintStatistics(double[] array, int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Array Length");
+            }
+
+            var totalSum = array[0];
+            var maxNumber = array[0];
+            var minNumber = array[0];
+
+            for (int i = 1; i < count; i++)
+            {
+                if (array[i] > maxNumber)
+                {
+                    maxNumber = array[i];
+                }
+
+                if (array[i] < minNumber)
+                {
+                    minNumber = array[i];
+                }
+
+                totalSum += array[i];
+            }
+
+            var averageNumber = totalSum / count;
+
+            this.PrintMax(maxNumber);
+            this.PrintMin(minNumber);
+            this.PrintAverage(averageNumber);
+        }
+
+        private void PrintAverage(double averageNumber)
+        {
+            Console.WriteLine("Average number: {0}", averageNumber);
+        }
+
+        private void PrintMin(double minNumber)
+        {
+            Console.WriteLine("Min number: {0}", minNumber);
+        }
+
+        private void PrintMax(double maxNumber)
+        {
+            Console.WriteLine("Max number: {0}", maxNumber);
+        }
+    }
+}
