f298808058bd59114ed07d01189d8f08fad7aa8c
Dilyan Traykov <dtraykov94@gmail.com>
2015-08-14 22:58:35 +0300

    'Refactoring' Homework Added

---
+++ High-Quality Code Homeworks/Refactoring/RotatingWalkInMatrix/CreateMatrix.cs
@@ -0,0 +1,15 @@
+﻿namespace Matrix
+{
+    using System;
+
+    internal class CreateMatrix
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insert number of rows for the matrix: ");
+            var n = int.Parse(Console.ReadLine());
+            var matrix = new Matrix(n);
+            Console.WriteLine(matrix);
+        }
+    }
+}
---
+++ High-Quality Code Homeworks/Refactoring/RotatingWalkInMatrix/Point.cs
@@ -0,0 +1,20 @@
+﻿namespace Matrix
+{
+    internal class Point
+    {
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y);
+        }
+    }
+}
