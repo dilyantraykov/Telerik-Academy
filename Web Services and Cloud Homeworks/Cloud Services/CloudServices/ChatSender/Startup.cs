6e0289a1b9ce8b431ac953dfc2b2dd4a49d99dd8
dilyantraykov <dtraykov94@gmail.com>
2015-11-17 10:08:53 +0200

    Part of Cloud Services homework added

---
+++ Web Services and Cloud Homeworks/Cloud Services/CloudServices/ChatSender/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ChatSender
+{
+    using System;
+    using IronMQ;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Client client = new Client(
+                "564ad77373d0cd00060000dd",
+                "sZYHg6S1N2cQU63ias0O");
+            Queue queue = client.Queue("Telerik Academy");
+            Console.WriteLine("Enter messages to be sent to Telerik Academy:");
+            while (true)
+            {
+                string msg = Console.ReadLine();
+                queue.Push(GetLocalIPAddress() + ": " + msg);
+                Console.WriteLine("Message sent to Telerik Academy.");
+            }
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Unknown IP");
+        }
+    }
+}
---
+++ Web Services and Cloud Homeworks/Cloud Services/CloudServices/SimpleChat/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleChat
+{
+    using System;
+    using System.Threading;
+    using IronMQ;
+    using IronMQ.Data;
+
+    public class Startup
+    {
+        static void Main()
+        {
+            Client client = new Client(
+                "564ad77373d0cd00060000dd",
+                "sZYHg6S1N2cQU63ias0O");
+            Queue queue = client.Queue("Telerik Academy");
+            Console.WriteLine("Listening for new messages from Telerik Academy:");
+            while (true)
+            {
+                Message msg = queue.Get();
+                if (msg != null)
+                {
+                    Console.WriteLine(msg.Body);
+                    queue.DeleteMessage(msg);
+                }
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
